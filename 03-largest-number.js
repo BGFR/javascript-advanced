edbb2deb8d5c66c947bdfd819cb424c963715b96
Bano Banov <banovb@hotmail.com>
2021-01-12 15:10:41 +0200

    Update 03-largest-number.js

--- 03-largest-number.js
+++ 03-largest-number.js
@@ -1,10 +1,10 @@
 //03. Largest Number 
 
 function largestNumber (a,b,c){
-let num = Math.max(a,b,c)
+let num = Math.max(a,b,c);
 
 console.log(`The largest number is ${num}.`);
 }
 
-largestNumber(5, -3, 16)
-largestNumber(-3, -5, -22.5)
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
