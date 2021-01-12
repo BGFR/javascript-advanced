6fedc18036792ae7a8b781bfab7bf5973df36585
Bano Banov <banovb@hotmail.com>
2021-01-12 15:10:06 +0200

    Update 01-echo-function.js

--- 01-echo-function.js
+++ 01-echo-function.js
@@ -5,5 +5,5 @@ console.log(input.length);
 console.log(input);
 }
 
-echoFunction('Hello, JavaScript!')
-echoFunction('strings are easy')
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
