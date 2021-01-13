7f53d909cf7579ac251c2b6a60eca9c9e32acea5
Bano Banov <banovb@hotmail.com>
2021-01-13 15:33:10 +0200

    Create pb-07-validity-checker.js

---
+++ 3-syntax-functions-statements/pb-07-validity-checker.js
@@ -0,0 +1,16 @@
+//PB-07. Validity Checker 
+function validityChecker(x1, y1, x2, y2) {
+    print(x1, y1, 0, 0);
+    print(x2, y2, 0, 0);
+    print(x1, y1, x2, y2);
+    function print(x1, y1, x2, y2) {
+        let result = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+        if (Number.isInteger(result)) {
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+        } else {
+            console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+        }
+    }
+}
+validityChecker(3, 0, 0, 4);
+validityChecker(2, 1, 1, 1);
