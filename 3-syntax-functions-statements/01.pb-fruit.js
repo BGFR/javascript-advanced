4db8f781f21ff4be7307c2005d1f3cdec22494b6
Bano Banov <banovb@hotmail.com>
2021-01-13 10:47:29 +0200

    Delete 01.pb-fruit.js

--- 3-syntax-functions-statements/01.pb-fruit.js
+++
@@ -1,7 +0,0 @@
-//PB-01. Fruit 
-function fruits (fruit,weight, priceKg) {
-    let totalPrice = (weight *(priceKg*10))/10000
-    console.log(`I need $${totalPrice.toFixed(2)} to buy ${(weight/1000).toFixed(2)} kilograms ${fruit}.`);
-}
-
-fruits('orange', 2500, 1.80)
