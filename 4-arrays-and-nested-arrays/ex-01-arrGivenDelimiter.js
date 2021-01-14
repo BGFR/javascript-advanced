da72b18b5174ac7c002642f2e895de095dcec85d
Bano Banov <banovb@hotmail.com>
2021-01-14 16:52:25 +0200

    Create ex-01-arrGivenDelimiter.js

---
+++ 4-arrays-and-nested-arrays/ex-01-arrGivenDelimiter.js
@@ -0,0 +1,13 @@
+//01. Print an Array with a Given Delimiter 
+function arrGivenDelimiter(arr,str) {
+
+    console.log(arr.join(str));
+}
+
+arrGivenDelimiter (['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+)
