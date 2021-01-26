481fbc17b1b0827e0cebdc5219af0aaa27291bab
Bano Banov <banovb@hotmail.com>
2021-01-26 22:20:40 +0200

    Update search.js

--- 06-DOM-introduction/ex-04.-Search-in-List/search.js
+++ 06-DOM-introduction/ex-04.-Search-in-List/search.js
@@ -1,14 +1,14 @@
 function search() {
-   let list = [...document.querySelectorAll('#towns>li')]
+   let list = [...document.querySelectorAll('#towns>li')];
    let input = document.querySelector('input').value;
    let result = document.getElementById('result');
 
-   let arr = []
+   let arr = [];
    for(let li of list){
       if(li.textContent.toUpperCase().includes(input.toUpperCase())){
-         li.style.fontWeight = 'bold'
-         li.style.textDecoration = 'underline'
-         arr.push(li)
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+         arr.push(li);
       }
    }
    return result.textContent = arr.length+' matches found';
