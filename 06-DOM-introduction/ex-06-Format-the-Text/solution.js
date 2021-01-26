67f061ca4eb7f3a96e3c653a3ee30c772478a19d
Bano Banov <banovb@hotmail.com>
2021-01-26 22:22:06 +0200

    Update solution.js

--- 06-DOM-introduction/ex-06-Format-the-Text/solution.js
+++ 06-DOM-introduction/ex-06-Format-the-Text/solution.js
@@ -4,44 +4,8 @@ function solve() {
   .filter(x => x!='');
 
   while(input.length){
-    let aParagraph = document.createElement('p')
-    aParagraph.innerHTML = input.splice(0,3)+'.'
-    document.querySelector('#output').appendChild(aParagraph)
+    let aParagraph = document.createElement('p');
+    aParagraph.innerHTML = input.splice(0,3)+'.';
+    document.querySelector('#output').appendChild(aParagraph);
   }
 }
-
-// // A-Manev github 100/100
-// function solve() {
-//   let input = document.getElementById('input').value
-//     .split('.')
-//     .filter(x => x != '');
-
-//   let result = '';
-//   while (input.length) {
-//     let currentParagraph = input.splice(0, 3);
-//     result += `<p>${currentParagraph.join('.')}.</p>`;
-//   }
-
-//   document.getElementById('output').innerHTML = result;
-// }
-
-// // 80/100 ot facebook grupata
-// function solve() {
-//   let inputText = document.getElementById('input').value;
-//   let sentenceList = Array.from(inputText.split(". "))
-//   let trial = []
-//   while(sentenceList.length>=3) {
-//     if (sentenceList[0]=="") {
-//       break;
-//     }
-//       trial = sentenceList.splice(0,3)
-//       let paragraph = document.createElement("P")
-//       paragraph.innerHTML = trial.join(".") + '.'
-//        document.getElementById('output').appendChild(paragraph) 
-//     }
-//     if (sentenceList.length < 3 && sentenceList[0]!="") {
-//       let paragraph = document.createElement("P")
-//       paragraph.innerHTML = sentenceList.join(".")
-//        document.getElementById('output').appendChild(paragraph) 
-//     }
-//   }
