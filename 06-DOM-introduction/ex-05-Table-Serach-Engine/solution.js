1dc272e2203252c571791cf78c631d36f3843d4e
Bano Banov <banovb@hotmail.com>
2021-01-26 22:21:09 +0200

    Update solution.js

--- 06-DOM-introduction/ex-05-Table-Serach-Engine/solution.js
+++ 06-DOM-introduction/ex-05-Table-Serach-Engine/solution.js
@@ -1,19 +1,19 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   let list = [...document.querySelectorAll('table tbody tr')]
+   let list = [...document.querySelectorAll('table tbody tr')];
    function onClick() {
       
-      let searchField = document.getElementById('searchField').value
+      let searchField = document.getElementById('searchField').value;
       console.log(searchField);
 
       for(let tr of list){
          if(tr.textContent.toLowerCase().includes(searchField.toLowerCase()) &&
          searchField !== ''){
             // tr.setAttribute('class', 'select') 
-            tr.classList.add('select') 
+            tr.classList.add('select') ;
          } else {
-            tr.removeAttribute('class')
+            tr.removeAttribute('class');
          }
       }
 
