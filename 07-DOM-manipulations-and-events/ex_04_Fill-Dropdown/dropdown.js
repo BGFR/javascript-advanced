88fd2344652000ef09f4e041fbceac42c8fa3c73
Bano Banov <banovb@hotmail.com>
2021-01-30 17:18:32 +0200

    Update dropdown.js

--- 07-DOM-manipulations-and-events/ex_04_Fill-Dropdown/dropdown.js
+++ 07-DOM-manipulations-and-events/ex_04_Fill-Dropdown/dropdown.js
@@ -1,37 +1,15 @@
-//mine
 function addItem() {
-    const inputText = document.getElementById('newItemText')
-    const inputValue = document.getElementById('newItemValue')
+    const inputText = document.getElementById('newItemText');
+    const inputValue = document.getElementById('newItemValue');
 
 
-        const outputOption =  document.createElement('option')
-        outputOption.textContent = inputText.value;
+        const outputOption =  document.createElement('option');
+        outputOption.textContent = inputText.value;;
         outputOption.value = inputValue.value;
-        let menu = document.getElementById('menu')
-        menu.appendChild(outputOption)
+        let menu = document.getElementById('menu');
+        menu.appendChild(outputOption);
 
-        inputText.value = ''
-        inputValue.value = ''
-
-}
-
-//hristomir asenov
-
-function addItem() {
- 
-    const $elements = {
-        inputText: document.getElementById('newItemText'),
-        inputValue: document.getElementById('newItemValue'),
-        selectMenu: document.getElementById('menu')
-    }
-
-    const $option = document.createElement('option')
-
-    $option.textContent = $elements.inputText.value;
-    $option.value = $elements.inputValue.value;
-
-    $elements.selectMenu.appendChild($option)
-
-    $elements.inputText.value = '';
-    $elements.inputValue.value = '';
+        inputText.value = '';
+        inputValue.value = '';
+;
 }
