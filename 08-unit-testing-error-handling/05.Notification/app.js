01f7968ac11bcd41f2054b037fef1f5ad3e0d377
Bano Banov <banovb@hotmail.com>
2021-02-11 16:12:49 +0200

    Update app.js

--- 08-unit-testing-error-handling/05.Notification/app.js
+++ 08-unit-testing-error-handling/05.Notification/app.js
@@ -1,16 +1,7 @@
-function notify() {
+function notify(message) {
    const notification = document.getElementById('notification');
-   let message = notification.textContent;
-   const btn = document.querySelector('button')
-   btn.addEventListener('click', function (ev) {
-      notification.style.display = notification.style.display == 'block'?
-      'none':'block';
-   })
-  }
+   notification.textContent = message;
+   notification.style.display = notification.style.display == 'block' ?
+      'none' : 'block';
+}
 
-  // function notify(message) {
-//  const notification = document.getElementById('notification');
-//  notification.textContent = message;
-//     notification.style.display = notification.style.display == 'block'?
-//     'none':'block';
-// }
